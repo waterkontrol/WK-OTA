d1e8b9ac5bb5906000239eff1700fc74c3484d2f
inyelber <inyelber@gmail.com>
2026-02-12 22:36:49 -0400

    version txt 2.8

--- WK-208_VBNI.ino
+++ WK-208_VBNI.ino
@@ -108,7 +108,7 @@ unsigned long tiempoAnterior = 0;
 static unsigned long ultimoPing100 = 0; // Control de frecuencia
 
 void realizarOTA(String url, String nuevaVersion) {
-  //Serial.println("si entra");
+  Serial.println("si entra");
   HTTPClient http;
   http.begin(url);
   http.addHeader("Cache-Control", "no-cache");
@@ -129,9 +129,13 @@ void realizarOTA(String url, String nuevaVersion) {
         
         // ===== GUARDAR VERSIÓN =====
         versionActual = nuevaVersion;
-        preferences.begin("ota", false);
+        /*preferences.begin("ota", false);
           preferences.putString("nueva_version"   , nuevaVersion); 
           preferences.putString("version_actual"  , versionActual);// ✅ GUARDA 1.7 EN "wifi"
+        preferences.end();*/
+
+        preferences.begin("ota", false);
+          preferences.putString("version_actual", nuevaVersion);  // ✅ GUARDA "2.8" en version_actual
         preferences.end();
 
         
